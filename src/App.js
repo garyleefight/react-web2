ba8d9a193de9907e89737e2da38945ede0b2ff8c
VividKnife <skang7@ncsu.edu>
2018-08-29 22:37:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello，world!!!B</h1>
+          <h1 className="App-title">Hello，world!!!你好花花</h1>
         </header>
       </div>
     );
